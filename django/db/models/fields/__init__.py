c85fd2e5621c02a0b721af845de1cac11baeaacc
jiahao.li <50519434+Garen-in-bush@users.noreply.github.com>
2020-12-10 12:00:04 +0800

    Update __init__.py
    remove some unnecessary code to avoid polluting my code

--- django/db/models/fields/__init__.py
+++ django/db/models/fields/__init__.py
@@ -341,7 +341,7 @@ class Field(RegisterLookupMixin):
         app_label = self.model._meta.app_label
         errors = []
         for alias in databases:
-            if router.allow_migrate(alias, app_label, model_name=self.model._meta.model_name):
+            if router.allow_migrate(alias, app_label):
                 errors.extend(connections[alias].validation.check_field(self, **kwargs))
         return errors
 
